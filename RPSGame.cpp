b601ca7ff5b5c03195e93834f9686250937cad85
anberns <anberns@gmail.com>
2017-02-13 16:28:28 -0500

    Create RPSGame.cpp

---
+++ RPSGame.cpp
@@ -0,0 +1,25 @@
+// header
+
+
+/**********************************************************************************************************************
+** RPSGame::computerChoice() 
+**
+*********************************************************************************************************************/
+void RPSGame::computerChoice()
+{
+  // need member variables to store last winner(1,2 or 3) and user's last choice(r,p or s), initialize to a value
+  
+  // first play, way to randomize?
+  
+  // all future plays
+  
+    if winner of last round
+      play tool that their choice would beat (same as what would be computer's last play)
+      
+     else if loser of last round
+      play tool that would beat their last choice
+      
+     else tie
+      play random tool?
+
+}
