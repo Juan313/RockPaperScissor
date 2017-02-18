7db53aa59cd9619a5e2a5e32d4b785be7bc52c7e
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2017-02-17 17:44:25 -0800

    Add setters and getters for Game class

--- RPSGame.hpp
+++ RPSGame.hpp
@@ -17,13 +17,42 @@
 class RPSGame
 {
 private:
-  Tool *human;
-  Tool *comp;
-  int human_wins;
-  int computer_wins;
-  int ties;
-  char human_last_tool;     // AI uses these variables in choosing next Tool
-  char comp_last_tool;
-  int last_winner;
-  
+    Tool *human;
+    Tool *comp;
+    int human_wins;
+    int computer_wins;
+    int ties;
+    char human_last_tool; // AI uses these variables in choosing next Tool
+    char comp_last_tool;
+    int last_winner;
+public:
+    // Set human tool
+    void set_human (Tool* human_object) {human = human_object;}
+    // Set computer tool
+    void set_comp (Tool* comp_object) {comp = comp_object;}
+    // Set human wins
+    void set_human_wins (int wins) {human_wins = wins;}
+    // Get human wins
+    int get_human_wins () {return human_wins;}
+    // Set computer wins
+    void set_computer_wins (int wins) {computer_wins = wins;}
+    // Get computer wins
+    int get_computer_wins () {return computer_wins;}
+    // Set number of ties
+    void set_ties (int ties_input) {ties = ties_input;}
+    // Get number of ties
+    int get_ties () {return ties;}
+    // Set last tool human used
+    void set_human_last_tool (char tool) {human_last_tool = tool;}
+    // Get last tool human used
+    char get_human_last_tool () {return human_last_tool;}
+    // Set last tool computer used
+    void set_comp_last_tool (char tool) {comp_last_tool = tool;}
+    // Get last tool computer used
+    char get_comp_last_tool () {return comp_last_tool;}
+    // Set last winner
+    void set_last_winner (int winner) {last_winner = winner;}
+    // Get last winner
+    int get_last_winner () {return last_winner;}
+    void computerChoice ();
 };
