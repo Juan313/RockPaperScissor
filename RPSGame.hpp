1855772881a25650bd41113888b25fc3f9bb5f4d
Aaron Berns <anberns@gmail.com>
2017-02-17 19:26:57 -0500

    Created RPSGame class

--- RPSGame.hpp
+++ RPSGame.hpp
@@ -1,1 +1,26 @@
+/************************************************************
+** File Name: RPSGame.hpp
+** Author 1: Nathaniel Bennet
+** Author 2: Aaron Berns
+** Author 3: Juan Du
+** Author 4: Christopher Dubbs
+** Author 5: Chieko Duncans
+** Author 6: Molly Johnson
+** Author 7: Armand Reitz
+** Date: 02/06/2016
+** CS 162 Group Project | Play Rock Paper, Scissors!!!
+** File discription: RPSGame class specification file
+**************************************************************/
+#include "Tool.hpp"
+#include "RPS.hpp"
 
+class RPSGame
+{
+private:
+  Tool *human;
+  Tool *comp;
+  int human_wins;
+  int computer_wins;
+  int ties;
+  
+};
